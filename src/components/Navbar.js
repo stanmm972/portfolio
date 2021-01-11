c1c539068f7e29938ba3cf469a041a68d03fb3aa
fred maurice <fmmadelon@gmail.com>
2021-01-11 18:14:29 -0500

    Services completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-light bg-custom-grey">
             <div className="container">
                 <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo"/></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
