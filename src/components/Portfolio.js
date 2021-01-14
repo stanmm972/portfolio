072852b0e2688a74dffcaaa703043be2a2ce706b
fred maurice <fmmadelon@gmail.com>
2021-01-14 13:27:23 -0500

    Portfolio component completed

---
+++ src/components/Portfolio.js
@@ -0,0 +1,59 @@
+import React from "react";
+import netflix from "../images/netflix.png";
+import cityGuide from "../images/city-guide-app.png";
+import portfolio from "../images/portfolio.png";
+import taskManager from "../images/task-manager.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
+import {PopupboxManager, PopupboxContainer} from "react-popupbox";
+
+
+const Portfolio = () => {
+  return (
+    <div className="portfolio-wrapper">
+      <div className="container">
+        <h1 className="text-uppercase text-center py-5">portfolio</h1>
+        <div className="image-box-wrapper row justify-content-center">
+          <div className="portfolio-image-box">
+            <img
+              className="portfolio-image"
+              src={netflix}
+              alt="Netflix Clone Project..."
+            />
+            <div className="overflow"></div>
+            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+          </div>
+          <div className="portfolio-image-box">
+            <img
+              className="portfolio-image"
+              src={cityGuide}
+              alt="City Guide Project..."
+            />
+            <div className="overflow"></div>
+            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+          </div>
+          <div className="portfolio-image-box">
+            <img
+              className="portfolio-image"
+              src={portfolio}
+              alt="Portfolio React and Material UI Project..."
+            />
+            <div className="overflow"></div>
+            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+          </div>
+          <div className="portfolio-image-box">
+            <img
+              className="portfolio-image"
+              src={taskManager}
+              alt="Task Manager React and Redux Project..."
+            />
+            <div className="overflow"></div>
+            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
